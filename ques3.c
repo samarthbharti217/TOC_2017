2e189be6cb9030e5f935329be896df41fb474b92
sb887 <sb887@snu.edu.in>
2017-03-14 00:40:32 +0530

    Updated Ques3.c

--- ques3.c
+++ ques3.c
@@ -6,9 +6,14 @@ void editorder(int*);
 int price =20;
 void main(){
 	int items[3]={0,0,0};
+	/*0-> Start state where order is taken from the user; 1-> Where the user csn view his order and proceed to pay; 2-> Where user inputs
+		currency into the machine until the final amount is matched; 3-> THis is the final state where the order of the user is accepted 
+		and the user is given his order; 4-> The user can chose to edit his order in this stage; 5->Dead state*/
+	int states[6]={0,1,2,3,4,5};
 	int tot=0,i, c;
 	while(1){
-		printf("__________________Enter the item you wish to add, or press 4 to proceed to pay, or 5 to cancel transaction____________________\n\n");
+		printf("--------State 0---------\n");
+		printf("Enter the item you wish to add, or press 4 to proceed to pay, or 5 to cancel transaction\n\n");
 		printf("1. Coffee\n");
 		printf("2. Tea\n");
 		printf("3. Soup\n");
@@ -17,20 +22,21 @@ void main(){
 			break;
 		}
 		else if(c==5){
+			printf("---------State 5---------\nTerminating...\n");
 			exit(0);
 		}
 		else if(c==1){
-			printf("Coffee added to bag\n");
+			printf("Coffee added to bag\n\n");
 			tot=tot+price;
 			items[0]++;
 		}
 		else if(c==2){
-			printf("Tea added to bag\n");
+			printf("Tea added to bag\n\n");
 			tot=tot+price;
 			items[1]++;
 		}
 		else if(c==3){
-			printf("Soup added to bag\n");
+			printf("Soup added to bag\n\n");
 			tot=tot+price;
 			items[2]++;
 		}
@@ -38,10 +44,12 @@ void main(){
 	int paid=0,x;
 	printf("________________________________________________________________________________________________________\n\n");
 	printbill(items);
-		while(1){
-		printf("Please enter your choice:\n1. Proceed to make payment\n2.Edit order\n3.Cancel transaction \n");
+	while(1){
+		printf("---------State 1---------\n");	
+		printf("Please enter your choice:\n1. Proceed to make payment\n2. Edit order\n3. Cancel transaction \nYour choice: ");
 		scanf("%d",&x);
 		if(x==3){
+			printf("\n--------State 5---------\nTerminating...\n");
 			exit(0);
 		}
 		else if(x==2){
@@ -55,46 +63,48 @@ void main(){
 	printf("Accepted denominations are: \nRs.1\tRs.2\tRs.5\tRs.10\n");
 	int input;
 	while(paid!=tot){
+		printf("State 2\n");
 		printf("Drop in money into machine\n");
 		scanf("%d",&input);
 		switch(input){
 			case 1:
 				paid=paid+1;
-				printf("Recieved Rs.1. Amount remaining=%d\n",tot-paid);
+				printf("Recieved Rs.1. Amount remaining=%d\n",(tot-paid));
 				break;
 			case 2:
 				if(paid+input>tot){
-					printf("Invalid amount. Exceeding total\n");
+					printf("Invalid amount. Exceeding total. Please collect money and try again.\n");
 					break;
 				}
 				paid=paid+2;
-				printf("Recieved Rs.2. Amount remaining=%d\n",tot-paid);
+				printf("Recieved Rs.2. Amount remaining=%d\n",(tot-paid));
 				break;
 			case 5:
 				if(paid+input>tot){
-					printf("Invalid amount. Exceeding total\n");
+					printf("Invalid amount. Exceeding total. Please collect money and try again\n");
 					break;
 				}
 				paid=paid+5;
-				printf("Recieved Rs.5. Amount remaining=%d\n",tot-paid);
+				printf("Recieved Rs.5. Amount remaining=%d\n",(tot-paid));
 				break;
 			case 10:
 				if(paid+input>tot){
-					printf("Invalid amount. Exceeding total\n");
+					printf("Invalid amount. Exceeding total. PLease collect money and try again\n");
 					break;
 				}
 				paid=paid+10;
-				printf("Recieved Rs.10. Amount remaining=%d\n",tot-paid);
+				printf("Recieved Rs.10. Amount remaining=%d\n",(tot-paid));
 				break;
 			default: printf("Invalid currency.\n");
 		}
 	}
+	printf("---------State 3---------\n");
 	printf("Thank you. Enjoy!");
 	
 }
 void printbill(int* items){
 	int i;
-	printf("Your bill is as follows:\n");
+	printf("\n\nYour bill is as follows:\n");
 	printf("Item\t\tUnits\t\tAmount\n");
 	for(i=0;i<3;i++){
 		if(items[i>0]){
@@ -115,42 +125,46 @@ void printbill(int* items){
 }
 void editorder(int* items){
 	int i,c;
-	printf("Please enter your choice of item you wish to edit or press 4 to view bill and exit:\n 1. Coffee\n2. Tea\n3. Soup\n");
-	scanf("%d",&c);
-	if(c==1){
-		printf("Enter amount of Coffee: ");
-		scanf("%d",&i);
-		if(i<0){
-			printf("\nCant have lesser coffee than 0\n");
+	while(1){
+		printf("\n---------State 4---------\n");
+		printf("Please enter your choice of item you wish to edit or press 4 to view bill and exit:\n 1. Coffee\n2. Tea\n3. Soup\n\nYour choice:");
+		scanf("%d",&c);
+		if(c==1){
+			printf("\nEnter amount of Coffee: ");
+			scanf("%d",&i);
+			if(i<0){
+				printf("\nCant have lesser coffee than 0\n");
+			}
+			else{
+				items[0]=i;
+				printf("\nNo. of cofee=%d and price=%d\n",i,i*20);
+			}
 		}
-		else{
-			items[0]=i;
-			printf("No. of cofee=%d and price=%d\n",i,i*20);
+		else if(c==2){
+			printf("\nEnter amount of Tea: ");
+			scanf("%d",&i);
+			if(i<0){
+				printf("\nCant have lesser tea than 0\n");
+			}
+			else{
+				items[1]=i;
+				printf("\nNo. of tea=%d and price=%d\n",i,i*20);
+			}
+		}
+		else if(c==3){
+			printf("\nEnter amount of Soup: ");
+			scanf("%d",&i);
+			if(i<0){
+				printf("\nCant have lesser soup than 0\n");
+			}
+			else{
+				items[2]=i;
+				printf("No. of soup=%d and price=%d\n",i,i*20);
+			}
+		}
+		else if(c==4){
+			printbill(items);
+			return;
 		}
 	}
-	else if(c==2){
-		printf("Enter amount of Tea: ");
-		scanf("%d",&i);
-		if(i<0){
-			printf("\nCant have lesser tea than 0\n");
-		}
-		else{
-			items[1]=i;
-			printf("No. of tea=%d and price=%d\n",i,i*20);
-		}
-	}
-	else if(c==3){
-		printf("Enter amount of Soup: ");
-		scanf("%d",&i);
-		if(i<0){
-			printf("\nCant have lesser soup than 0\n");
-		}
-		else{
-			items[2]=i;
-			printf("No. of soup=%d and price=%d\n",i,i*20);
-		}
-	}
-	else if(c==4){
-		printbill(items);
-	}		
 }
